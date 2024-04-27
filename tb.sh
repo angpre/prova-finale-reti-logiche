85a1e4fc73ce0991ac56b3abc3cc9f8cc1d2264d
Angelo Prete <angelo.prete@skywarder.eu>
2024-04-27 12:20:36 +0200

    Fixed tb script and added .gitignore

--- tb.sh
+++ tb.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-ghdl -a project_reti_logiche.vhd project_tb.vhd
+mkdir -p build
+cd build
+ghdl -a ../project_reti_logiche.vhd ../project_tb.vhd
 ghdl -e project_tb
 ghdl -r project_tb --vcd=wave.vcd
